f13591a8e695f377d16a153d719f3759a9281c35
Bruno Konrad <bruno.skon@gmail.com>
2023-06-05 16:34:37 +0200

    Fix tsc errors

--- client/src/features/orders/lib/addressToString.ts
+++ client/src/features/orders/lib/addressToString.ts
@@ -1,4 +1,4 @@
-import type { OrderAddress } from "@/lib/api";
+import type { OrderAddress } from "./ordersApi";
 
 export function addressToString(address: OrderAddress): string {
   return `${address.street} ${address.zipCode} ${address.city}`;
